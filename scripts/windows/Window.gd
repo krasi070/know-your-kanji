ff1b17874f3dd6050ff912fb5fed8472493c24b1
Krasimir Balchev <krasi0707@gmail.com>
2021-06-03 20:00:35 +0300

    Reorganize menubar

--- scripts/windows/Window.gd
+++ scripts/windows/Window.gd
@@ -1,24 +1,38 @@
 extends MarginContainer
 
 var curr_window : String
+var add_kanji_scene : PackedScene
+var review_scene : PackedScene
+var windows : Dictionary
+
+onready var review_button := $"../MenuContainer/LeftContainer/Review"
+onready var add_kanji_button := $"../MenuContainer/LeftContainer/AddKanji"
 
 
 func _ready():
-	$"../MenuContainer/Reviews".connect(
-			"opened_window", self, "open_window")
-	$"../MenuContainer/Kanji".connect(
-			"opened_window", self, "open_window")
+	review_scene = preload("res://scenes/ReviewWindow.tscn")
+	review_button.connect(
+			"button_up", self, "open_window", [review_scene, "Review", review_button])
+	add_kanji_scene = preload("res://scenes/AddKanjiWindow.tscn")
+	add_kanji_button.connect(
+			"button_up", self, "open_window", [add_kanji_scene, "Add Kanji", add_kanji_button])
 
 
-func open_window(window, window_name) -> void:
+func open_window(scene: PackedScene, window_name: String, button: Button) -> void:
 	# Only open new window if it is differen from the currently open one
 	if curr_window != window_name:
-		free_children()
-		add_child(window)
+		hide_children()
+		if windows.has(window_name):
+			windows[window_name].show()
+		else:
+			var instance := scene.instance()
+			add_child(instance)
+			windows[window_name] = instance
+			button.text = "・" + button.text
 		curr_window = window_name
 
 
-func free_children() -> void:
+func hide_children() -> void:
 	var children = get_children()
 	for i in range(children.size()):
-		children[i].queue_free()
+		children[i].hide()
