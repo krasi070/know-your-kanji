f3b8f4e352d99299a3f2bba0baf78b7122a8b849
Krasimir Balchev <krasi0707@gmail.com>
2021-06-08 00:19:45 +0300

    Add open file folder functionality

--- scripts/windows/Window.gd
+++ scripts/windows/Window.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+const PATH_TO_OPEN := "user://"
+
 var curr_window : String
 var add_kanji_scene : PackedScene
 var review_scene : PackedScene
@@ -8,6 +10,7 @@ var search_scene : PackedScene
 onready var review_button := $"../MenuContainer/LeftContainer/Review"
 onready var add_kanji_button := $"../MenuContainer/LeftContainer/AddKanji"
 onready var search_button := $"../MenuContainer/RightContainer/SearchButton"
+onready var open_file_button := $"../MenuContainer/LeftContainer/OpenFile"
 onready var search_bar := $"../MenuContainer/RightContainer/SearchBar"
 
 
@@ -15,34 +18,41 @@ func _ready():
 	# Review window set-up
 	review_scene = preload("res://scenes/ReviewWindow.tscn")
 	review_button.connect(
-			"button_up", self, "open_window", [review_scene, "Review"])
+			"button_up", self, "_open_window", [review_scene, "Review"])
 	# Add kanji window set-up
 	add_kanji_scene = preload("res://scenes/AddKanjiWindow.tscn")
 	add_kanji_button.connect(
-			"button_up", self, "open_window", [add_kanji_scene, "Add Kanji"])
+			"button_up", self, "_open_window", [add_kanji_scene, "Add Kanji"])
 	# Search window set-up
 	search_scene = preload("res://scenes/SearchWindow.tscn")
-	search_button.connect("button_up", self, "open_search_window")
+	search_button.connect("button_up", self, "_open_search_window")
+	# Open file set-up
+	open_file_button.connect("button_up", self, "_open_file")
 	
 
-func open_window(scene: PackedScene, window_name: String) -> void:
+func _open_window(scene: PackedScene, window_name: String) -> void:
 	# Only open new window if it is differen from the currently open one
 	if curr_window != window_name:
-		free_children()
+		_free_children()
 		var instance := scene.instance()
 		add_child(instance)
 		curr_window = window_name
 
 
-func open_search_window() -> void:
-	free_children()
+func _open_search_window() -> void:
+	_free_children()
 	var instance := search_scene.instance()
 	add_child(instance)
 	curr_window = "Search"
 	instance.search(search_bar.text)
 
 
-func free_children() -> void:
+func _open_file() -> void:
+	var globalized_path := ProjectSettings.globalize_path(PATH_TO_OPEN)
+	OS.shell_open(globalized_path)
+
+
+func _free_children() -> void:
 	var children = get_children()
 	for i in range(children.size()):
 		children[i].queue_free()
